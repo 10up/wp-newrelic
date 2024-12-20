36dd3e089bd903698c44c76baa717a07a0c2044d
Dharmesh Patel <dspatel44@gmail.com>
2024-12-20 16:22:20 +0530

    Add some settings related tests.

--- tests/cypress/e2e/admin.test.js
+++ tests/cypress/e2e/admin.test.js
@@ -12,4 +12,33 @@ describe("Admin can login and open dashboard", () => {
     cy.deactivatePlugin("wp-newrelic");
     cy.activatePlugin("wp-newrelic");
   });
+
+  it( 'Can see "New Relic" menu and Can visit "New Relic" settings page.', () => {
+		cy.visit( '/wp-admin/' );
+
+		// Check ClassifAI menu.
+		cy.get( '#adminmenu li#menu-tools ul.wp-submenu li' ).contains(
+			'New Relic'
+		);
+
+		// Check Heading
+		cy.visit( '/wp-admin/tools.php?page=wp-nr-settings' );
+		cy.get( 'h1' ).contains( 'New Relic for WordPress' ).should( 'exist' );
+	} );
+
+  it( 'Can save "New Relic" settings.', () => {
+		cy.visit( '/wp-admin/tools.php?page=wp-nr-settings' );
+		cy.get( 'input[name="wp_nr_capture_urls"' ).check();
+    cy.get( 'input#submit' ).click();
+
+    cy.visit( '/wp-admin/tools.php?page=wp-nr-settings' );
+    cy.get( 'input[name="wp_nr_capture_urls"' ).should( 'be.checked' );
+
+    cy.visit( '/wp-admin/tools.php?page=wp-nr-settings' );
+		cy.get( 'input[name="wp_nr_capture_urls"' ).uncheck();
+    cy.get( 'input#submit' ).click();
+
+    cy.visit( '/wp-admin/tools.php?page=wp-nr-settings' );
+    cy.get( 'input[name="wp_nr_capture_urls"' ).should( 'not.be.checked' );
+	} );
 });
