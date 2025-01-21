e012cc0029b6789111947a0c447faa15e79732bb
Peter Wilson <git-commit-by@peterwilson.cc>
2025-01-21 12:15:32 +1100

    Use persistent URL for installation.

--- tests/bin/initialize.sh
+++ tests/bin/initialize.sh
@@ -10,11 +10,11 @@ npm run env run tests-wordpress -- sudo apt-get update
 npm run env run tests-wordpress -- sudo apt-get install -y wget
 
 # Step 3: Download the New Relic agent
-npm run env run tests-wordpress -- sudo wget -L -O newrelic-php5-11.4.0.17-linux.tar.gz https://download.newrelic.com/php_agent/release/newrelic-php5-11.4.0.17-linux.tar.gz
+npm run env run tests-wordpress -- sudo wget -L -O newrelic-php5-11.5.0.18-linux.tar.gz https://download.newrelic.com/php_agent/archive/11.5.0.18/newrelic-php5-11.5.0.18-linux.tar.gz
 
 # Step 4: Extract and install
-npm run env run tests-wordpress -- tar -xzf newrelic-php5-11.4.0.17-linux.tar.gz
-npm run env run tests-wordpress sudo NR_INSTALL_USE_CP_NOT_LN=1 NR_INSTALL_SILENT=0 ./newrelic-php5-11.4.0.17-linux/newrelic-install install
+npm run env run tests-wordpress -- tar -xzf newrelic-php5-11.5.0.18-linux.tar.gz
+npm run env run tests-wordpress sudo NR_INSTALL_USE_CP_NOT_LN=1 NR_INSTALL_SILENT=0 ./newrelic-php5-11.5.0.18-linux/newrelic-install install
 
 # Step 5: Restart the web server
 npm run env run tests-wordpress sudo /etc/init.d/apache2 reload
